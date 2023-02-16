069ea6f0009641bb72c84e4e86852963dc8c1ab1
Jorge Castillo <xdicarousxd@gmail.com>
2023-02-16 15:11:23 -0500

    add repeatModal and functionalitis

--- src/ModalForRepeat/index.jsx
+++ src/ModalForRepeat/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './modalRepeat.css'
 
 export const ModalForRepeat = ({ changeValueRepeat }) => {
 	return (
 		<>
 			<div className="modal-repeat">
-				<h2>Ya tiene un ToDo con ese título</h2>
-				<p>por favor cambie el título</p>
-				<button onClick={changeValueRepeat}>Okey</button>
+				<div>
+					<h2>Ya tiene un ToDo con ese título</h2>
+					<p>por favor cambie el título</p>
+					<button onClick={changeValueRepeat}>Okey</button>
+				</div>
 			</div>
 		</>
 	);
